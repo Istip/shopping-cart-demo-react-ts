fe2e9965aafaa70349c9b09a998d0daed39cedca
Istip <isticsek@gmail.com>
2022-07-06 22:28:39 +0300

    Boilerplate finished

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,29 @@
 import React from 'react';
-import pizzas from '../data/pizzas.json';
-import Pizza from './Pizza';
 import style from './App.module.css';
 
 const App = () => {
+  const center = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 'calc(100vh - 20px)',
+  };
+
   return (
     <div className={style.container}>
-      <ul>
-        {pizzas.map((pizza) => (
-          <Pizza pizza={pizza} key={pizza.id} />
-        ))}
-      </ul>
+      <div style={center}>
+        <div>
+          <h2>
+            Basic <abbr title="https://reactjs.org/">React</abbr> ➕{' '}
+            <abbr title="https://www.typescriptlang.org/">Typescript</abbr>{' '}
+            project setup.
+          </h2>
+          <br />
+          <div style={{ textAlign: 'center' }}>
+            <small>CSS Modules and build script included!</small>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
