327306b04c9d0f77d7a4164db7c1b8f40be507b8
Istip <isticsek@gmail.com>
2022-07-06 19:36:03 +0300

    Babel configuration and type checking created

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
-function log(str: string) {
+export function log(str: string) {
   console.log(str);
 }
 
 log('Hello world!');
+
+class A {
+  greeting = 'Hello vili!';
+}
+
+log(new A().greeting);
