0a9c0303d88f51a999696be303a6b289639c9ae1
Istip <isticsek@gmail.com>
2022-07-07 08:22:51 +0300

    Added svg support and logo

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import pizzas from '../data/pizzas.json';
 import Pizza from './Pizza';
-import style from './App.module.css';
+import styles from './App.module.css';
+import Logo from '../assets/pizza.svg';
 
 const App = () => {
   return (
-    <div className={style.container}>
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <Logo width={120} height={120} />
+        <div className={styles.siteTitle}>Delicious Pizza</div>
+      </div>
       <ul>
         {pizzas.map((pizza) => (
           <Pizza pizza={pizza} key={pizza.id} />
--- src/custom.d.ts
+++ src/custom.d.ts
@@ -2,3 +2,10 @@ declare module '*.css' {
   const css: { [key: string]: string };
   export default css;
 }
+
+declare module '*.svg' {
+  const ReactComponent: React.ComponentType<
+    React.SVGProps<SVGSAttributes<SVGElement>>
+  >;
+  export default ReactComponent;
+}
